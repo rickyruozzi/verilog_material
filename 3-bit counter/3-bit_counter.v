568d0ec03052ad868659a06b8ffa353199ef517f
ruozziriccardo4 <ruozziriccardo@4gmail.com>
2025-10-23 22:49:24 +0200

    commmenti modulo contatore a 3 bit

--- 3-bit counter/3-bit_counter.v
+++ 3-bit counter/3-bit_counter.v
@@ -1,15 +1,15 @@
 module three_bit_counter #(parameter N=8)( //ricordarsi # per impostare i parametri
-    input clk,
-    input reset,
-    input enable, 
-    output reg [N-1:0] count
+    input clk,  //segnale di clock
+    input reset, //segnale di reset basso attivo
+    input enable, //abilitazione contatore
+    output reg [N-1:0] count //canale di output
 );
-    always @(negedge clk or negedge reset) begin
-        if (reset == 1'b0) begin
-            count<=0;
+    always @(negedge clk or negedge reset) begin //sensibile al clock e alla variazione di reset
+        if (reset == 1'b0) begin //se reset è attivo azzera il contatore
+            count<=0;   //assegnazione non bloccante
         end
-        else if(enable) begin
-            count <= count+1;
+        else if(enable) begin //se enable e attivo incrementa il contatore visto che se siamo qui il trigger proviene dal clock
+            count <= count+1; //assegnazione non bloccante
         end
     end
 endmodule
