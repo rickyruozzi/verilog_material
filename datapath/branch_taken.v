61ef5207d08d7f8694c7639dd71bc8d0fc2bd814
ruozziriccardo4 <ruozziriccardo@4gmail.com>
2025-11-23 17:38:29 +0100

    inverted branch taken

--- datapath/branch_taken.v
+++ datapath/branch_taken.v
@@ -1,12 +1,12 @@
 module branch_taken();
     always @(inst_funct3 or alu_result_equal_zero) begin
     case (inst_funct3)
-    `FUNCT3_BRANCH_EQ: take_branch = !alu_result_equal_zero;
-    `FUNCT3_BRANCH_NE: take_branch = alu_result_equal_zero;
-    `FUNCT3_BRANCH_LT: take_branch = !alu_result_equal_zero;
-    `FUNCT3_BRANCH_GE: take_branch = alu_result_equal_zero;
-    `FUNCT3_BRANCH_LTU: take_branch = !alu_result_equal_zero;
-    `FUNCT3_BRANCH_GEU: take_branch = alu_result_equal_zero;
+    `FUNCT3_BRANCH_EQ: take_branch = alu_result_equal_zero;
+    `FUNCT3_BRANCH_NE: take_branch = !alu_result_equal_zero;
+    `FUNCT3_BRANCH_LT: take_branch = alu_result_equal_zero;
+    `FUNCT3_BRANCH_GE: take_branch = !alu_result_equal_zero;
+    `FUNCT3_BRANCH_LTU: take_branch = alu_result_equal_zero;
+    `FUNCT3_BRANCH_GEU: take_branch = !alu_result_equal_zero;
     default: take_branch = 1'bx;
     endcase
     end
